654762a9ab56e611520fb0c4a64661e6cbfd5620
Dinkar Kumar <109248473+DINKAR202@users.noreply.github.com>
2023-11-05 18:42:57 +0530

    Update RoleDice.jsx

--- src/components/RoleDice.jsx
+++ src/components/RoleDice.jsx
@@ -7,7 +7,7 @@ const RoleDice = ({currentDice, roleDice}) => {
       <div className="dice"
         onClick={roleDice}
       >
-        <img src={`/images/dice/dice_${currentDice}.png`} alt={`dice_${currentDice}`} />
+        <img src={`/public/images/dice/dice_${currentDice}.png`} alt={`dice_${currentDice}`} />
         <p>Click on Dice to roll</p>
       </div>
     </DiceContainer>
