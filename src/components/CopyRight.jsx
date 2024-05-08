246ddaeb71be888104b3b64853c8dc8f5de7bf09
Dinkar Kumar <109248473+DINKAR202@users.noreply.github.com>
2024-05-09 00:07:26 +0530

    Update CopyRight.jsx

--- src/components/CopyRight.jsx
+++ src/components/CopyRight.jsx
@@ -4,7 +4,7 @@ const CopyRight = () => {
   return (
     <Copy>
       <small>
-        Copyright &copy; 2023 <b><a className="name" href="https://www.instagram.com/its_me_aditi65/">AditiDev.</a></b>
+        Copyright &copy; 2023 <b><a className="name" href="#">Dinkar</a></b>
       </small>
     </Copy>
   );
